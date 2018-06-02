33ff1802c0a614be9993ac41543d1ad52f154809
yjs <yangjiangsonggit@163.com>
2018-06-03 00:22:26 +0800

    递归

---
+++ src/main/java/com/yjs/datastructureandalgorithm/AdvancedSort/Partition.java
@@ -0,0 +1,57 @@
+package com.yjs.datastructureandalgorithm.AdvancedSort;
+
+
+/**
+ * 划分
+ * create by jiangsongy on 2018/6/2
+ */
+public class Partition {
+
+    private long[] sortArr = {2,44,33,17,4,454,23,5,9,78,24,54,8,45,27,412,63,12,34};
+
+    //枢纽
+    private long pivot = 34;
+
+    private long[] partitonSort(int left,int right,long pivot,long[] arr){
+
+        int leftPar = left - 1;
+        int rightPar = right + 1;
+
+        while (true){
+            //寻找比枢纽大的元素下标
+            while (leftPar < right && arr[++leftPar] < pivot)
+                ;
+
+            while (rightPar > left && arr[--rightPar] > pivot)
+                ;
+
+            if (leftPar >= rightPar){
+                break;
+            }else {
+                this.swap(leftPar,rightPar,arr);
+            }
+
+        }
+        return arr;
+    }
+
+    private void swap(int leftPar, int rightPar, long[] arr) {
+        long tmp = arr[leftPar];
+        arr[leftPar] = arr[rightPar];
+        arr[rightPar] = tmp;
+    }
+
+    private static void printSortedArr(long[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ",");
+        }
+    }
+
+    public Partition(){
+        this.printSortedArr(this.partitonSort(0,sortArr.length - 1,this.pivot,this.sortArr));
+    }
+
+    public static void main(String[] args) {
+        new Partition();
+    }
+}
---
+++ src/main/java/com/yjs/datastructureandalgorithm/AdvancedSort/ShellSort.java
@@ -0,0 +1,51 @@
+package com.yjs.datastructureandalgorithm.AdvancedSort;
+
+
+/**
+ * 希尔排序
+ * create by jiangsongy on 2018/6/2
+ */
+public class ShellSort {
+
+    private long[] sortArr = {2,44,33,17,4,454,23,5,9,78,24,54,8,45,27,412,63,12,34};
+
+    private long[] shellSort(long[] arr){
+
+        int h = 1;
+        //增量序列最大值
+        while (h <= arr.length / 3){
+            h = h * 3 + 1;
+        }
+
+        while (h > 0){
+            for (int i = h; i < arr.length; i += h) {
+                //对当前元素做插入排序
+                if (arr[i] < arr[i - h]){
+                    long tmp = arr[i];
+                    int j = i - h;
+                    while (j >= 0 && arr[j] > tmp){
+                        arr[j + h] = arr[j];
+                        j -= h;
+                    }
+                    arr[j + h] = tmp;
+                }
+            }
+            h = (h - 1) / 3;
+        }
+        return arr;
+    }
+
+    private static void printSortedArr(long[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ",");
+        }
+    }
+
+    public ShellSort(){
+        this.printSortedArr(this.shellSort(this.sortArr));
+    }
+
+    public static void main(String[] args) {
+        new ShellSort();
+    }
+}
